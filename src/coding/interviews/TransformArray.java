d4496ed661c0ea00f127337ac1d938171e58e79b
art84parthu <art84parthu@gmail.com>
2015-03-20 19:17:29 +0530

    Transform array problem description

---
+++ src/coding/interviews/TransformArray.java
@@ -0,0 +1,24 @@
+package coding.interviews;
+
+public class TransformArray {
+
+	/**
+	 * Given an input array of N elements, 
+	 * at a given time all the elements can be either incremented or decremented by 1. 
+	 * 
+	 * Find minimum number of steps required to transform the array such that
+	 * all elements are equal. 
+	 * 
+	 * [11, 3, 7, 1] (initial array)
+	 * [10, 4, 6, 2] (after step 1)
+	 * [9, 5, 7, 3] (after step 2)
+ 	 * [8, 6, 6, 4] (after step 3)
+	 * [7, 7, 5, 5] (after step 4)
+	 * [6, 6, 6, 6] (after step 5)
+	 * 
+	 * Some in some arrays it may not be possible.
+	 * for example it is not possible with [1,4,7] to equalise elements to one number. 
+	 * In Such cases it should return -1
+	 * 
+	 */
+}
